735a3f59c0feaf9b0790235febe7535ee9815777
jsfernando <jsfernando@gmail.com>
2021-04-20 12:11:24 -0300

    updated concepts props.children

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ function App() {
   return (
     <> 
     <h1>Hello Nlw#5</h1>
-    <Button title="Button 1"/>
-    <Button title="Button 2"/>
-    <Button title="Button 3"/>
-    <Button title="Button 4"/>
+    <Button>Button 1</Button>
+    <Button>Button 2</Button>
+    <Button>Button 3</Button>
+    <Button>Button 4</Button>
+    
     </>
   );
 }
