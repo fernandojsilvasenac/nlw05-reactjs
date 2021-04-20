dcf31b0a4e7ff352031ec876a70205f1ded163c8
jsfernando <jsfernando@gmail.com>
2021-04-20 12:28:17 -0300

    updated in State

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,23 @@
+//usar um conceito chamado estado
+import { useState } from 'react';
+
 export default function Button(props){
+  const [counter, setCounter] = useState(1);
+  // let counter = 1;
+
+  // [estado,  alterarEstado] //2a opção é uma função
+
+  function increment() {
+    // counter = counter +1
+    // console.log(counter);
+    setCounter(counter +1);
+  }
+
   return (
-    <button>{props.children}</button>
+    <>
+    <span>{counter}</span>
+    <button onClick={increment}>{props.children}</button> 
+    <br/>
+    </>
   )
 }
